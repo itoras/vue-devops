9a8b7beb481275d8995fe7c5f003e4a6dffa39ba
itoras <lsh104305@naver.com>
2021-06-30 01:19:40 +0900

    테스트 통과 확인

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to itoras App" />
+  <HelloWorld msg="Welcome to itoras App 테스트 성공" />
 </template>
 
 <script>
